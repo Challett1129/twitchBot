c45c0f12fd7ae994a76186044142cb0910756ac5
Collin <collinhallett@gmail.com>
2022-03-09 01:40:29 -0600

    clancy bot can now console.log people's chats

--- index.js
+++ index.js
@@ -2,43 +2,50 @@ const tmi = require('tmi.js');
 require('dotenv').config();
 
 // Define configuration options
-const opts = {
-  identity: {
-    username: BOT_USERNAME,
-    password: OAUTH_TOKEN
-  },
-  channels: [
-    CHANNEL_NAME
-  ]
-};
+// const opts = {
+//   identity: {
+//     username: BOT_USERNAME,
+//     password: OAUTH_TOKEN
+//   },
+//   channels: [
+//     CHANNEL_NAME
+//   ]
+// };
 
 // Create a client with our options
-const client = new tmi.client(opts);
+const client = new tmi.client({
+    channels:['clancy_b0t']
+});
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
+client.on('message', (channel, tags, message, self) => {
+    console.log(tags);
+    console.log(`${tags['display-name']}: ${message} `)
+});
 client.on('connected', onConnectedHandler);
 
 // Connect to Twitch:
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+// function onMessageHandler (target, context, msg, self, tags) {
+//   if (self) { return; } // Ignore messages from the bot
 
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
+//   console.log(`${tags['display-name']}: ${message}`)
 
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    console.log(context);
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
-}
+//   // Remove whitespace from chat message
+//   const commandName = msg.trim();
+
+//   // If the command is known, let's execute it
+//   if (commandName === '!dice') {
+//     const num = rollDice();
+//     console.log(context);
+//     client.say(target, `You rolled a ${num}`);
+//     console.log(`* Executed ${commandName} command`);
+//   } else {
+//     console.log(`* Unknown command ${commandName}`);
+//   }
+// }
 
 // Function called when the "dice" command is issued
 function rollDice () {
