8ce6030a5f392fb4d26a0dbce602931504af9c9b
Collin <collinhallett@gmail.com>
2022-03-09 23:06:59 -0600

    bot is working with three commands. In a usable state

--- index.js
+++ index.js
@@ -17,28 +17,48 @@ const tmiConfig = {
   }
 };
 
+const commands = {
+  donate: {
+    response: `https://www.paypal.com/paypalme/sgtxenas any and all donations go to further the stream or support paying the bills`
+  },
+  dice: {
+    response: (user) => `${user} rolled ${value = rollDice()}`
+  },
+  clancy: {
+    response: `Hello! I am clancy_b0t, the chatbot inspired by sgt_xenas' cat. I like to chew ankles and help out the stream!`
+  }
+}
+
 // Create a client with our options
 const client = new tmi.client(tmiConfig);
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 
 // Connect to Twitch:
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) return // Ignore messages from the bot
-
-  const [raw, command, argument]
-  console.log(`${context.username}: ${msg}`)
-}
+client.on('message', (channel, tags, message, self) => {
+  if ( self ) return;
+  if(message.charAt(0) === '!') {
+    const [raw, command, argument] = message.match(regexpCommand)
+  
+    const { response } = commands[command] || {}
+  
+    if( typeof response === 'function') {
+      client.say(channel, response(tags.username))
+    } else if (typeof response === 'string') {
+      client.say(channel, response);
+    }
+  }
+})
 
 // Function called when the "dice" command is issued
 function rollDice () {
   const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
+  value = Math.floor(Math.random() * sides) + 1;
+  return value
 }
 
 // Called every time the bot connects to Twitch chat
